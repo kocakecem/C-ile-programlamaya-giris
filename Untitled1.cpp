26c5939175c42b152a337e4dd8e2e015791a7288
kocakecem <76884409+kocakecem@users.noreply.github.com>
2021-01-02 23:28:30 +0300

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,112 @@
+#include  <stdio.h>
+
+main(){
+	
+  //printf("Merhaba C\n"); // ilk kodum 
+  //printf("Merhaba C"); // 
+  int  sayi1 = 5; // 4 byte
+  /*
+  char birharf = 'A'; // 1 byte
+  printf("birharf = %c", birharf );
+  float pi = 3.14;
+  printf("pi sayisi = %f", pi );
+  */
+
+  int sayi2 = 4;  /*
+  printf("5 + 4 = %d\n", sayi1 + sayi2 );
+  printf("5 - 4 = %d\n", sayi1 - sayi2 );
+  printf("5 * 4 = %d\n", sayi1 * sayi2 );
+  printf("5 / 4 = %d\n", sayi1 / sayi2 );
+  printf("5 %% 4 = %d\n", sayi1 % sayi2 ); */
+  
+  scanf("%d", &sayi1);
+  scanf("%d", &sayi2); 
+  
+  
+  
+  int toplam = sayi1 + sayi2;
+  printf("%d", toplam);
+  
+  /*
+  || : veya 
+  */
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+}
